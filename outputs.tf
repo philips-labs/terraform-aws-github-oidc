9be4cc0decaa7dcc797df8ada17177df0c7cb2d7
codezninja <github@ezbon.xyz>
2022-04-03 14:25:19 -0400

    feature: output conditions added to IAM role

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "role" {
   description = "The crated role that can be assumed for the configured repository."
   value       = var.repo != null ? aws_iam_role.main[0] : null
 }
+
+output "conditions" {
+  description = "The assume conditions added to the role."
+  value       = local.conditions
+}
