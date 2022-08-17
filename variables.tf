b0ef8a544f75d6c80d882c59fa62898506677395
Miro Toman <44134862+mtmn@users.noreply.github.com>
2022-08-17 16:58:37 +0200

    feat: add support for adding custom policies (#37)

--- variables.tf
+++ variables.tf
@@ -64,3 +64,9 @@ variable "role_permissions_boundary" {
   type        = string
   default     = null
 }
+
+variable "role_policy_arns" {
+  description = "List of ARNs of IAM policies to attach to IAM role"
+  type        = list(string)
+  default     = []
+}
